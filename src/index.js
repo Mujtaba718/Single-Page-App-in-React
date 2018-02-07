6a1b70fe6eaef01c2a04153faba7505a56f4a3af
Mujtaba Iqbal <mujtaba.iq@gmail.com>
2018-02-06 22:38:13 -0500

    First commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Main } from './Main';
+import "./index.css";
+
+const app = document.getElementById('root');
+ReactDOM.render(
+	<Main />, 
+	app
+);
